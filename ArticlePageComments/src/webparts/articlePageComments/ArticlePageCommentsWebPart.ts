203014adb0afa630a98690853fbabda5ccf28c73
JS-PADOAN <71463081+JS-PADOAN@users.noreply.github.com>
2020-09-18 17:37:02 +0200

    WebPart ArticlePageComments ok

--- ArticlePageComments/src/webparts/articlePageComments/ArticlePageCommentsWebPart.ts
+++ ArticlePageComments/src/webparts/articlePageComments/ArticlePageCommentsWebPart.ts
@@ -11,14 +11,6 @@ import * as strings from 'ArticlePageCommentsWebPartStrings';
 import ArticlePageComments from './components/ArticlePageComments';
 import { IArticlePageCommentsProps } from './components/IArticlePageCommentsProps';
 
-import IPageComment from "./components/IPageComment";
-import {
-  SPHttpClient,
-  HttpClient,
-  SPHttpClientResponse,
-  IHttpClientOptions,
-  HttpClientResponse
-} from "@microsoft/sp-http";
 
 export interface IArticlePageCommentsWebPartProps {
   description: string;
@@ -26,54 +18,18 @@ export interface IArticlePageCommentsWebPartProps {
 
 export default class ArticlePageCommentsWebPart extends BaseClientSideWebPart<IArticlePageCommentsWebPartProps> {
 
-  private _baseUrl: string;
-  private _listId: string;
-  private _listItemId: string;
-  private _spHttpClient: SPHttpClient;
-  private _httpClient: HttpClient;
-
-  public componentWillMount(): void {
-    this._baseUrl = this.context.pageContext.web.absoluteUrl;
-    this._listId =
-      this.context.pageContext.list &&
-      this.context.pageContext.list.id.toString();
-    this._listItemId =
-      this.context.pageContext.listItem &&
-      this.context.pageContext.listItem.id.toString();
-    this._spHttpClient = this.context.spHttpClient;
-    this._httpClient = this.context.httpClient;
-
-    let comments: IPageComment[] = await this._getTop5PageComments();
-
-  }
-
   public render(): void {
     const element: React.ReactElement<IArticlePageCommentsProps> = React.createElement(
       ArticlePageComments,
       {
         description: this.properties.description, 
+        context : this.context
       }
     );
 
     ReactDom.render(element, this.domElement);
   }
 
-  private async _getTop5PageComments(): Promise<IPageComment[]> {
-    const pageCommentsEndpoint: string = `${this._baseUrl}/_api/web/lists('${this._listId}')/GetItemById(${this._listItemId})/Comments?$top=5&$inlineCount=AllPages`;
-
-    const response: SPHttpClientResponse = await this._spHttpClient.get(pageCommentsEndpoint, SPHttpClient.configurations.v1);
-    const responseJson: any = await response.json();
-    const comments: IPageComment[] = responseJson.value.map(c => {
-      const comment: IPageComment = {
-        id: c.id,
-        author: c.author.name,
-        comment: c.text
-      };
-      return comment;
-    });
-
-    return comments;
-  }
 
 
   protected onDispose(): void {
--- ArticlePageComments/src/webparts/articlePageComments/components/ArticlePageComments.tsx
+++ ArticlePageComments/src/webparts/articlePageComments/components/ArticlePageComments.tsx
@@ -2,20 +2,85 @@ import * as React from 'react';
 import styles from './ArticlePageComments.module.scss';
 import { IArticlePageCommentsProps } from './IArticlePageCommentsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { IArticlePageCommentsState } from './IArticlePageCommentsState';
 
-export default class ArticlePageComments extends React.Component<IArticlePageCommentsProps, {}> {
+
+import IPageComment from "./IPageComment";
+import {
+  SPHttpClient,
+  HttpClient,
+  SPHttpClientResponse,
+  IHttpClientOptions,
+  HttpClientResponse
+} from "@microsoft/sp-http";
+
+export default class ArticlePageComments extends React.Component<IArticlePageCommentsProps,IArticlePageCommentsState> {
+  
+  
+
+  constructor(props: IArticlePageCommentsProps, state: IArticlePageCommentsState) {  
+    super(props);    
+
+    this.state = {  
+      comments: []
+    };     
+  }
+  private _baseUrl: string;
+  private _listId: string;
+  private _listItemId: string;
+  private _spHttpClient: SPHttpClient;
+  
+  public componentWillMount(): void {
+
+  let c = this.props.context;
+
+    this._baseUrl = c.pageContext.web.absoluteUrl;
+    this._listId =
+      c.pageContext.list &&
+      c.pageContext.list.id.toString();
+    this._listItemId =
+     c.pageContext.listItem &&
+     c.pageContext.listItem.id.toString();
+    this._spHttpClient = c.spHttpClient; 
+  }
+
+  public componentDidMount(): void {  
+    this._getTop5PageComments().then (c=> {
+          this.setState({ comments: c });
+        }
+      );
+  }
+
+  private async _getTop5PageComments(): Promise<IPageComment[]> {
+    const pageCommentsEndpoint: string = `${this._baseUrl}/_api/web/lists('${this._listId}')/GetItemById(${this._listItemId})/Comments?$top=5&$inlineCount=AllPages`;
+
+    const response: SPHttpClientResponse = await this._spHttpClient.get(pageCommentsEndpoint, SPHttpClient.configurations.v1);
+    const responseJson: any = await response.json();
+    const comments: IPageComment[] = responseJson.value.map(c => {
+      const comment: IPageComment = {
+        id: c.id,
+        author: c.author.name,
+        comment: c.text
+      };
+      return comment;
+    });
+
+    return comments;
+  }
+  
   public render(): React.ReactElement<IArticlePageCommentsProps> {
+  
+    let items = this.state.comments.map((item) => <li>{item.comment}</li>);   
+    
     return (
       <div className={ styles.articlePageComments }>
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+              <span className={ styles.title }>Here are the {this.state.comments.length} comment(s)</span>
+              <ul>
+                {items}
+              </ul>
             </div>
           </div>
         </div>
